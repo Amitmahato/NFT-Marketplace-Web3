c5e4f198382b4eeca98a2e3d430d41f41ef90d8c
Amit Mahato <amit643.mahato@gmail.com>
2022-11-07 16:55:00 +0545

    4.2 Add Profile page & use next Link to navigate to profile pages in nav bar menu item

---
+++ pages/profile.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import { BaseLayout } from "../components";
+
+export const Profile: NextPage = () => {
+  return (
+    <BaseLayout>
+      <div className="text-black">Profile</div>
+    </BaseLayout>
+  );
+};
+export default Profile;
