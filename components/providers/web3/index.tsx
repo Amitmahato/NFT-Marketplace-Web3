a217fd9990f460c27971d6be309fa4d10852420d
Amit Mahato <amit643.mahato@gmail.com>
2022-11-08 17:55:06 +0545

    5.5 Window Ethereum - declare global interface for Window exending ethereum - initialize web3 state with ethereum on mount

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -2,6 +2,7 @@ import React, {
   createContext,
   FunctionComponent,
   useContext,
+  useEffect,
   useState,
 } from "react";
 import { createDefualtWeb3State, Web3State } from "./utils";
@@ -12,6 +13,20 @@ const Web3Provider: FunctionComponent<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [web3Api, setWeb3Api] = useState<Web3State>(createDefualtWeb3State());
+
+  useEffect(() => {
+    const initWeb3 = () => {
+      const ethereum = window.ethereum;
+      setWeb3Api({
+        ...web3Api,
+        ethereum,
+        isLoading: false,
+      });
+    };
+
+    initWeb3();
+  }, []);
+
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
   );
