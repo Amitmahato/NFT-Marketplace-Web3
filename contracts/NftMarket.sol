4dbe5d6a6e3efa349be48c404a82616847aac330
Amit Mahato <amit643.mahato@gmail.com>
2022-11-16 21:21:57 +0545

    6.3 Inherit ERC721URIStorage and define ERC721 NFT Collection name & symbol

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract NftMarket {}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NftMarket is ERC721URIStorage {
+    // ERC721(<NFT Collection Name>, <NFT Collection Symbol>)
+    constructor() ERC721("CreaturesNFT", "CNFT") {}
+}
